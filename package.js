22590940b76b942d024cfe981016668a97c762a1
Evgenii Popov <linegeji@gmail.com>
2021-04-30 09:52:50 +0300

    WiP

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
 	// Brief, one-line summary of the package.
 	summary: "Replacement for Meteor's MongoDB oplog implementation",
 	// URL to the Git repository containing the source code for this package.
-	git: 'https://github.com/cult-of-coders/redis-oplog',
+	git: 'https://github.com/sharekey/redis-oplog',
 	// By default, Meteor will default to using README.md for documentation.
 	// To avoid submitting documentation, set this field to null.
 	documentation: 'README.md',
@@ -17,7 +17,7 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-	api.versionsFrom('1.12');
+	api.versionsFrom('1.11');
 	api.use([
 		'underscore',
 		'ecmascript',
