66d6f1df35883445f02d7b96755bedd808c52718
Evgenii Popov <linegeji@gmail.com>
2021-07-29 05:24:37 +0300

    v2.2.0
    * Reverted changes to work with sentinels
    * Removed detection of port from env, port is hardcoded to default one
    * Commented out tests dependencies of which somehow became incompatible with each other after new release of meteor

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'skadmin:redis-oplog',
-    version: '2.1.10',
+    version: '2.2.0',
     // Brief, one-line summary of the package.
     summary: "Replacement for Meteor's MongoDB oplog implementation",
     // URL to the Git repository containing the source code for this package.
@@ -36,28 +36,28 @@ Package.onUse(function(api) {
     api.mainModule('redis-oplog.client.js', 'client');
 });
 
-Package.onTest(function(api) {
-    api.use('skadmin:redis-oplog');
-
-    // extensions
-    api.use('aldeed:collection2@3.0.0');
-    api.use('reywood:publish-composite@1.5.2');
-    api.use('natestrauser:publish-performant-counts@0.1.2');
-    api.use('socialize:user-presence@0.4.0');
-
-    api.use('ecmascript');
-    api.use('tracker');
-    api.use('mongo');
-    api.use('random');
-    api.use('accounts-password');
-    api.use('matb33:collection-hooks@0.8.4');
-    api.use('alanning:roles@1.2.16');
-
-    api.use(['meteortesting:mocha']);
-
-    api.mainModule('testing/main.server.js', 'server');
-    api.addFiles('testing/publishComposite/boot.js', 'server');
-    api.addFiles('testing/optimistic-ui/boot.js', 'server');
-
-    api.mainModule('testing/main.client.js', 'client');
-});
+// Package.onTest(function(api) {
+//     api.use('skadmin:redis-oplog');
+//
+//     // extensions
+//     api.use('aldeed:collection2@3.0.0');
+//     api.use('reywood:publish-composite@1.5.2');
+//     api.use('natestrauser:publish-performant-counts@0.1.2');
+//     api.use('socialize:user-presence@0.4.0');
+//
+//     api.use('ecmascript');
+//     api.use('tracker');
+//     api.use('mongo');
+//     api.use('random');
+//     api.use('accounts-password');
+//     api.use('matb33:collection-hooks@0.8.4');
+//     api.use('alanning:roles@1.2.16');
+//
+//     api.use(['meteortesting:mocha']);
+//
+//     api.mainModule('testing/main.server.js', 'server');
+//     api.addFiles('testing/publishComposite/boot.js', 'server');
+//     api.addFiles('testing/optimistic-ui/boot.js', 'server');
+//
+//     api.mainModule('testing/main.client.js', 'client');
+// });
